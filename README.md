967820f937f7bba367aaacddc6cf7670f162c6a9
Glitch (catywhiny3) <none>
2019-09-26 09:37:28 +0000

    👾🛢 Updated with Glitch

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-# API Project: URL Shortener Microservice for freeCodeCamp
+# Exercise Tracker REST API
 
+#### A microservice project, part of Free Code Camp's curriculum
 
 ### User Stories
 
-1. I can POST a URL to `[project_url]/api/shorturl/new` and I will receive a shortened URL in the JSON response. Example : `{"original_url":"www.google.com","short_url":1}`
-2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.example.com(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`. *HINT*: to be sure that the submitted url points to a valid site you can use the function `dns.lookup(host, cb)` from the `dns` core module.
-3. When I visit the shortened URL, it will redirect me to my original link.
-
-
-#### Creation Example:
-
-POST [project_url]/api/shorturl/new - body (urlencoded) :  url=https://www.google.com
-
-#### Usage:
-
-[this_project_url]/api/shorturl/3
-
-#### Will redirect to:
-
-https://www.freecodecamp.org/forum/
+1. I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and _id.
+2. I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+3. I can add an exercise to any user by posting form data userId(_id), description, duration, and optionally date to /api/exercise/add. If no date supplied it will use current date. Returned will the the user object with also with the exercise fields added.
+4. I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). Return will be the user object with added array log and count (total exercise count).
+5. I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
--- server.js
+++ server.js
@@ -1,37 +1,48 @@
-'use strict';
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const cors = require('cors')
 
-var cors = require('cors');
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
-var app = express();
+app.use(cors())
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
 
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
